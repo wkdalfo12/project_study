a7b152146da3713ad555ba26606f5755aa1a7437
wkdalfo12 <wkdalfo12@hanmail.net>
2016-08-26 18:48:48 +0900

    8.26

--- html/JYP/js/gal.js
+++ html/JYP/js/gal.js
@@ -1,56 +1,70 @@
 $(function(){
-    
+
     $('.photos').children('ul').children('li').click(function(){
-        
+
         var idx = $(this).index();
-      blindFn();
-       
-    
+        blindFn();
+        black();
+
+    // // 클릭했을때 이미지뜨게
     $('.bb').children('p').eq(idx).children('img').fadeIn()
-    
-   
-    
+
       $('.disnone').css({
           'zIndex':'100'
       })
-      
-       
-       
-       // //   x버튼
-     
+
+
+
+
+
+    // //   x버튼
       $('.disnone .bb p').eq(idx).children('a').css({
           'display':'inline-block'
       }).parent('p').siblings().children('a').css({
           'display':'none'
       })
-      
-      
+    // //   x버튼 누르면 닫히게
       $('.disnone .bb p').on('click',$('.btn'),function(){
-          
           $('.disnone').css({
           'zIndex':'-10'
             })
           $('.disnone .bb').children('p').eq(idx).children('img').fadeOut();
-          
+
           $('.blind').fadeOut(function(){
               $(this).remove();
           })
+
+          $('.black').fadeOut(function(){
+              $(this).remove();
+          })
               })
-    
+
        function blindFn(){
            var bl_Tag ="";
            bl_Tag += '<div class="blind">';
            bl_Tag += '</div>'
-           
+
            $('body').prepend(bl_Tag);
-           
-           $('.blind').show(1000);
+
+           $('.blind').show();
        }
-       
+
+  // 검은 막  
+       function black(){
+            var add ='';
+                add += '<div class="black">'
+                add += '</div>'
+
+                $('body').prepend(add)
+                $('html, body').css({'overflow':'hidden'});
+                $('.black').fadeTo(0,1)
+
+            }
+
         return false;
-       
+
     })
-   
-   
-   
+
+
+
 })
