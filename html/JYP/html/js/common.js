21bbc3d4f326b15c83cb00d442b58e22180c3690
wkdalfo12 <wkdalfo12@hanmail.net>
2016-09-02 17:27:01 +0900

    9.2

--- html/JYP/html/js/common.js
+++ html/JYP/html/js/common.js
@@ -1,13 +1,7 @@
 $(function(){
 
 $('.nav > button').click(function(){
-
-
-//    if(!$('.nav > ul').is('.on')) $('.nav > ul').addClass('on').fadeIn(400).slideDown(800);
-//    else $('.nav > ul').removeClass('on').fadeOut(400).slideUp(800);
-
-$('.nav ul').toggle()
-
+    $('.nav ul').toggle()
 })
 
 var aa = true;
@@ -18,7 +12,6 @@ $(window).resize(function(){
            'display':'block'
        })
        aa = false;
-
    }
    else if( $(window).width() < 888 && !aa){
        $('.nav ul').css({
--- html/JYP/html/js/gal.js
+++ html/JYP/html/js/gal.js
@@ -1,43 +1,27 @@
 $(function(){
 
-    // $(window).risize(function(){
-    //
-    //
-    // })
-
     $('.photos').children('ul').children('li').click(function(){
-
-        var idx = $(this).index();
-        blindFn();
-        black();
+            var idx = $(this).index();
+            blindFn();
+            black();
 
     // // 클릭했을때 이미지뜨게
     $('.bb').children('p').eq(idx).children('img').fadeIn()
-
-      $('.disnone').css({
-          'zIndex':'100'
-      })
-
-
-  // 이미지 클릭 했을때 x닫는 아이콘 넣는법 :
-  // 스크립트에서 offset값과 이미지의 위드값 뽑아낸 후 x한테 부여
-    var imgwidth = $('.bb p').children('img').eq(idx).width();
-    var pp = $('.bb p').children('img').eq(idx).offset().left;
-    var xwidth = imgwidth+pp
-  console.log()
+          $('.disnone').css({
+              'zIndex':'100'
+          })
 
     // //   x버튼
-      $('.disnone .bb p').eq(idx).children('a').css({
+    $('.disnone .bb p').eq(idx).children('a').css({
           'display':'block',
-
-      }).parent('p').siblings().children('a').css({
+    }).parent('p').siblings().children('a').css({
           'display':'none'
-      })
+    })
 
     // //   x버튼 누르면 닫히게
-      $('.disnone .bb p').on('click',$('.btn'),function(){
+    $('.disnone .bb p').on('click',$('.btn'),function(){
           $('.disnone').css({
-          'zIndex':'-10'
+              'zIndex':'-10'
             })
           $('.disnone .bb').children('p').eq(idx).children('img').fadeOut();
 
@@ -48,20 +32,18 @@ $(function(){
           $('.black').fadeOut(function(){
               $(this).remove();
           })
-              })
+    })
 
-       function blindFn(){
+    function blindFn(){
            var bl_Tag ="";
            bl_Tag += '<div class="blind">';
            bl_Tag += '</div>'
-
            $('body').prepend(bl_Tag);
-
            $('.blind').show();
-       }
+    }
 
-  // 검은 막
-       function black(){
+    // 검은 막
+    function black(){
             var add ='';
                 add += '<div class="black">'
                 add += '</div>'
@@ -69,13 +51,7 @@ $(function(){
                 $('body').prepend(add)
                 $('html, body').css({'overflow':'hidden'});
                 $('.black').fadeTo(0,1)
-
-            }
-
+    }
         return false;
-
     })
-
-
-
 })
